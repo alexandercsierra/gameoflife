433ca33b45bf7e893a8eb319d77360d3ff006297
Alexander Sierra <alexandercsierra@gmail.com>
2020-05-27 12:19:40 -0400

    have a pixel grid

--- src/components/Animated.js
+++ src/components/Animated.js
@@ -7,17 +7,60 @@ const Animated = () => {
         if (canvasRef){
             const canvas = document.getElementById('my-canvas')
             console.log('canvas', canvasRef)
-            const context = canvas.getContext('2d');
-            const imageData = context.getImageData(0,0,canvas.width, canvas.height)
-            const screenBuffer = imageData.data;
-            console.log('screenbuffer', imageData)
-
-
-            context.putImageData(imageData, 0, 0)
+            const ctx = canvas.getContext('2d');
+            const imageData = ctx.getImageData(0,0,canvas.width, canvas.height)
+            console.log('width', canvas.width)
+            if(imageData){
+                makeVerticalLines(imageData, ctx)
+                makeHorizontalLines(imageData, ctx)
+                makeBlackSquare(imageData, ctx)
+            }
             
-    
+            
         }
     },[])
+    
+    const makeVerticalLines = (imageData, context) => {
+            let data = imageData.data
+            console.log('data', data)
+            let numPixels = data.length/4
+            for (let i=0; i< numPixels; i +=25){
+                data[i*4 + 3] = 255;
+            }
+            
+        context.putImageData(imageData, 0, 0);
+
+    }
+
+    const makeHorizontalLines = (imageData, context) => {
+            let data = imageData.data
+            console.log('data', data)
+            let numPixels = data.length/500
+            for (let i = 10000; i< 500000; i+=10000){
+
+                for (let j=i; j< i+500; j++ ){
+                    data[j*4+3] = 255;
+                }
+            }
+            
+        context.putImageData(imageData, 0, 0);
+
+    }
+
+    const makeBlackSquare = (imageData, context) => {
+        // let data = imageData.data
+        // let numPixels = data.length/40000
+        // for (let i=0; i< numPixels; i++){
+        //     data[i*4+3] = 255;
+
+        // }
+
+        context.beginPath();
+        context.arc(50, 50, 50, 0, 2 * Math.PI);
+        context.fill();
+
+        context.putImageData(imageData, 0, 0);
+    }
     
     const canvasRef = useRef(null);
     
@@ -33,7 +76,7 @@ const Animated = () => {
     return(
         <div>
             <h3>Testing Animations</h3>
-            <canvas id='my-canvas' ref={canvasRef} width={500} height={500} style={{background: 'black'}}/>
+            <canvas id='my-canvas' ref={canvasRef} width={500} height={500} style={{background: 'white', border: '1px solid red'}}/>
         </div>
 
     )
